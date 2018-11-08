f2fe6faf595d2a193dab3524db86aa3b9248e199
qqzhw <qqzhw@DESKTOP-ZHW>
2018-11-08 23:19:29 +0800

    3

--- ICIMS.Service/BaseData/IBuyCategoryService.cs
+++ ICIMS.Service/BaseData/IBuyCategoryService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ICIMS.Model.BaseData;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,16 @@ namespace ICIMS.Service.BaseData
 {
     public interface IBuyCategoryService
     {
+        Task<ResultData<BuyCategory>>  GetAllCategorys(string No="",string Name="", int pageIndex = 0, int pageSize = int.MaxValue);
+
+         
+        Task<BuyCategory> GetById(int input);
+
+         
+        Task CreateOrUpdate(BuyCategory input);
+
+ 
+        Task Delete(int input);
 
     }
 }
