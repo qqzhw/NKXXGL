2b3d17f0c0eb78dc0b3303e7b2d7eca128666dcb
qqzhw <qqzhw@DESKTOP-ZHW>
2018-11-12 21:36:39 +0800

    view

---
+++ Modules/ICIMS.Modules.BusinessManages/ViewModels/ReViewDefineViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICIMS.Modules.BusinessManages.ViewModels
+{
+    class ReViewDefineViewModel
+    {
+    }
+}
